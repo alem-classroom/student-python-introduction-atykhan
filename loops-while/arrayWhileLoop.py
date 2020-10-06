0ed87fe996fe801edc6810a08bbc977e9ee2233b
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-06 15:47:05 +0600

    trial

--- loops-while/arrayWhileLoop.py
+++ loops-while/arrayWhileLoop.py
@@ -1,2 +1,11 @@
 def insert_squares(arr, num):
     # add square of numbers from 1 to num to the list named arr and return list
+    i = 1
+    while i <= 5: 
+    	k = i*i
+    	arr.append(k)
+    	i = i + 1
+    return(arr)
+#a = [1, 2, 3]
+#n = 5
+#print(insert_squares(a,n))
