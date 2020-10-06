dde5b073b0b91eed9ff4c133b888f8a5fa7ec53a
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-06 17:09:52 +0600

    final'

--- lists/lists.py
+++ lists/lists.py
@@ -18,6 +18,6 @@ def sort_list(list):
 def reverse(list):
     # return reversed list
 	return(list.reverse())
-#st = [[1, -3, 0, 342], ["hello", "world", [213]]]
-#a = 1
+#lst = [[1, -3, 0, 342], ["hello", "world", [213]],["dsd", "ass", [123]], [[5],[4],"d"]]
+#a = 2
 #print(delete_elem_from_list(lst,a))
