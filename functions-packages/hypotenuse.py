88327b1911f32a229ec7ce4b7ad2c1d84d44bbf9
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-06 15:55:23 +0600

    trial

--- functions-packages/hypotenuse.py
+++ functions-packages/hypotenuse.py
@@ -1,3 +1,6 @@
 import math   
 def get_hypotenuse(catet1, catet2):  
     # calculate hypotenuse of right triangle based on 2 catets and return it
+    c = math.sqrt(catet1*catet1+catet2*catet2)
+    return(c)
+#print(get_hypotenuse(5,12))
