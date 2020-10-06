8566058a9ca5831b20c2eda7f35992dcc160fd4e
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-06 15:41:16 +0600

    trial

--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,11 +1,5 @@
-def reverse_dict(dict):
-    # swap keys and values within dict and return dict
 #dict = {'first': 'a', 'second': 'b'}
-	for k in dict.keys():
-		print(dict.get(k),k)
-	#print(dict.get(k))
-	#for k in dict.keys():
-		#if (v == k):	
-		#	print(k)
-		#else:
-			#break
+def reverse_dict(dict):
+	inv_map = {v: k for k, v in dict.items()}
+	return(inv_map)
+#print(reverse_dict(dict))
