ae6531758b7c68b1169a2d5214f1c9c0d43c0d95
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-06 16:59:10 +0600

    trial

--- lists/lists.py
+++ lists/lists.py
@@ -5,9 +5,10 @@ def add_elem_to_list(list, elem):
     # add elem to list and return the list
     list.append(elem)
     return(list)
-def delete_elem_from_list(list, index):
+def delete_elem_from_list(list, index = -1):
     # delete element from list, such that its index is index
-    return(list.pop(index+1))
+    lis.pop(index)
+    return(list)
 def count_elements_in_list(list, x):
     # count elements in the list that are equal to x and return the count
     return(list.count(x))
