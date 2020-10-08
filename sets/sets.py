243a1b70938ccbc7540a450b253e9a00fee2ee91
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-08 12:14:45 +0600

    trial

--- sets/sets.py
+++ sets/sets.py
@@ -1,18 +1,19 @@
 def size_of_set(set):
     # return size of set  
+    return(len(set))
 def is_elem_in_set(set, elem):
     # return true if elem exists in set, false otherwise
-
+    return(elem in set)
 def are_sets_equal(first_set, second_set):
     # return true if sets have the same elements inside, otherwise false
-
+    return(first_set == second_set)
 def add_elem_to_set(set, elem):
     # add elem to the set if elem does not exist in set, and return the set
     # if elem exists in set, return set
-
+    return(set.add(elem))
 def remove_elem_if_exists(set, elem):
     # remove elem from set if it exists, and return the set
-
+    return(set.remove(elem))
 def delete_first_element(set):
     # delete first elemenent of set
-    
+ 	return(set.pop())   
