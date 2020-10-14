48d6e061979df5ef2c0b65ba88467a55eebe5b81
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-14 13:08:15 +0600

    traiL

--- lists/lists.py
+++ lists/lists.py
@@ -7,7 +7,12 @@ def add_elem_to_list(list, elem):
     return(list)
 def delete_elem_from_list(list, index = -1):
     # delete element from list, such that its index is index
-    return(list.pop(index))
+    if index == 38:
+        return(list.pop(38))
+    elif index == 40:
+        return(list.pop(40))
+    else:
+        return(list.pop(index))
 def count_elements_in_list(list, x):
     # count elements in the list that are equal to x and return the count
     return(list.count(x))
