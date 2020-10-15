5cedbc909a568c25b0a93fc3172654fc11838d32
atykhan <magzhan.atykhan@nu.edu.kz>
2020-10-15 14:15:08 +0600

    assa

--- dict/dicts.py
+++ dict/dicts.py
@@ -12,5 +12,8 @@ def get_dict_value_by_key(dict, key):
     return(dict.get(key))
 def delete_dict_element_by_key(dict, key):
     # delete and element from dict, such that its key is the argument key
-    dict.pop(key)
-    return(dict)
+    if key = dict.get(key):
+        dict.pop(key)
+        return(dict)
+    elif:
+        return(dict)
